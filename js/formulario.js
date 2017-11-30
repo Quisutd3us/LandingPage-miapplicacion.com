f7e9f68eafd9a9317de6256d9457d6e70e6c933d
David Nariño <dnarino@gmail.com>
2017-11-30 12:06:43 -0500

    formulario: validacion con ajax

--- js/formulario.js
+++ js/formulario.js
@@ -1,9 +1,33 @@
-// console.log("todo ok");
-$('#formulario').submit(function(event){
+
+$('#formulario').submit(function (event) {
     event.preventDefault();
     enviar();
 })
 
-function enviar(){
-    console.info('enviando...');
+function enviar() {
+    var datos = $('#formulario').serialize(); //Toma los datos name y los lleva a un arreglo
+    $.ajax({
+        type: "post",
+        url: "formulario.php",
+        data: datos,
+        success: function (texto) {
+            if (texto == 'exito') {
+                correcto();
+            } else {
+                phperror(texto);
+            }
+        }
+    })
+
+    function correcto() {
+        console.log('entro mtf');
+        $('#mensajeExitoso').removeClass('d-none');
+        $('#mensajeError').addClass('d-none');
+    }
+
+    function phperror(texto) {
+        $('#mensajeError').removeClass('d-none');
+        $('#mensajeError').html(texto);
+
+    }
 }
---
+++ js/formulario.php
@@ -0,0 +1,54 @@
+
+<?php 
+$error ="";
+
+//validando el campo nombre
+if(empty($_POST['nombre'])){
+    $error = 'Ingrese un Nombre<br>';
+}else{
+    //Flitrando campo nombre
+    $nombre = $_POST['nombre'];
+    $nombre = filter_var($nombre,FILTER_SANITIZE_STRING);
+}
+
+if(empty($_POST['email'])){
+    $error.="Ingrese un Email<br>";
+}else{
+    $email = $_POST['email'];
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error.= 'Ingrese un Email Valido de la forma dnarino@gmail.com';
+    }else{
+        $email = filter_var($email, FILTER_SANITIZE_STRING);
+    }
+}
+
+if(empty($_POST['comentario'])){
+    $error = 'Ingrese un comentario<br>';
+}else{
+    //Flitrando campo nombre
+    $comentario = $_POST['comentario'];
+    $comentario = filter_var($comentario,FILTER_SANITIZE_STRING);
+}
+
+// Creando el Email
+
+// creando el cuerpo
+$cuerpo = 'Nombre: '.$nombre.'/n';
+$cuerpo .= 'Email: '.$email.'/n';
+$cuerpo .= 'Mensaje: '.$mensaje.'/n';
+
+
+// creando el destinatario
+
+$enviarA = 'dnarino@gmail.com';
+$asunto = 'Nuevo Mensaje del Landing Page';
+
+
+if($error == ""){
+    $success= mail($enviarA,$asunto,$cuerpo,'de: '.$email);
+    echo 'exito';
+}else{
+    echo 'error';
+}
+
+?>
